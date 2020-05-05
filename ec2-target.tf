e09edc5508ff83772192bc9468ccf6b6458dee39
Mikesh Khanal <44762372+mkhanal1@users.noreply.github.com>
2020-05-05 14:26:50 +0530

    Update ec2-target.tf

--- ec2-target.tf
+++ ec2-target.tf
@@ -6,16 +6,19 @@ resource "aws_instance" "myec2_2" {
    vpc_security_group_ids = [ aws_security_group.target-sg.id ]
    depends_on = [aws_instance.myec2]
  }
+
 module "QualysScanning" {
   source  = "matti/resource/shell"
   command = "curl -H 'X-Requested-With: Curl' -u '${var.username}:${var.password}' -X 'POST' -d \"action=launch&scan_title=My+Vulnerability+Scan&ip=${aws_instance.myec2_2.private_ip}&option_id=${var.optionprofile}&iscanner_name=${var.scannername}\" ${var.baseurl}/api/2.0/fo/scan/"
   depends = [module.QualysAddIP]
 }
+   
 module "QualysAddIP" {
   source  = "matti/resource/shell"
   command = "curl -H 'X-Requested-With: Curl' -u '${var.username}:${var.password}' -X 'POST' -d \"action=add&enable_vm=1&ips=${aws_instance.myec2_2.private_ip}\" ${var.baseurl}/api/2.0/fo/asset/ip/;sleep 900"
   depends = [aws_instance.myec2_2]
   }
+   
 #######################################################
 # Outputs
 #######################################################
